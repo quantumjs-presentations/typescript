b043f2e3fc198f4ddfd5f9556ef042cf1e1408c0
Nikos Katsikanis <nkatsikanis@gmail.com>
2016-11-22 18:15:48 +0000

    Deploy to gh-pages script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+exists=`git show-ref refs/heads/gh-pages`
+if ![ -n "$exists" ]; then
+    echo 'branch exists!'
+    git checkout -b "gh-pages"
+else
+    git checkout "gh-pages"
+fi
+
+git merge master
+git add dist/* -f
+git commit -a -m 'build files'
+git push origin gh-pages
+git checkout master
